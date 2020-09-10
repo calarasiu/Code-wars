97df6c28c3a314b6f90fcabddbb4f44067ba34d5
cornelia chirila <cornelia.calarasiu@yahoo.it>
2020-09-10 08:55:09 +0100

    return the longest word

---
+++ Code-wars/longest_word.js
@@ -0,0 +1,12 @@
+function LongestWord(sen) { 
+  const punctuationless = sen.replace(/[^\w\s]/g,"");
+  console.log(punctuationless);
+  sen = punctuationless.split(" ")
+  console.log(sen);
+  sen.sort(function(a,b){return(b.length-a.length)});
+  // code goes here  
+  return sen[0]; 
+
+}
+
+console.log(LongestWord("I am! !curious"));
