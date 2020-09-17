ce1357358b563724dc5c9f9a0ac75a016c0390bf
cornelia chirila <cornelia.calarasiu@yahoo.it>
2020-09-17 08:24:16 +0100

    reverse words

--- Code-wars/reverseWords.js
+++ Code-wars/reverseWords.js
@@ -1,15 +1,27 @@
-const reverseWords = (arr) => {
-  let i=0;
-  const arrWords = []
-  arr.map=(element)=>{
-    if(element === " "){
-      i++;
-      return element;
-    } else{
-      arrWords[i]+=element;
-    }
+function reverseWords(arr){
+  
+ reverse(0, arr.length-1, arr);
+
+  currentWordStartIndex = 0;
+
+  for(i=0; i<=arr.length; i++){
+    if(i===arr.length || arr[i]===" "){
+      reverse(currentWordStartIndex, i-1, arr);
+      currentWordStartIndex = 1+ i;
+    }    
   }
-  return arrWords;
+  return arr;
+}
+
+function reverse(startLine, finishLine, toReverse){
+  let temp = 0;
+  while(startLine<finishLine){
+    temp = toReverse[startLine];
+    toReverse[startLine] = toReverse[finishLine];
+    toReverse[finishLine] = temp;
+    startLine++;
+    finishLine--;
+  }
 }
 
 console.log(reverseWords([ 'c', 'a', 'k', 'e', ' ','p', 'o', 'u', 'n', 'd', ' ','s', 't', 'e', 'a', 'l' ]));
