6c583f83b77d8c5e8c2209cb25f133a18f7e6433
cornelia chirila <cornelia.calarasiu@yahoo.it>
2020-10-11 21:46:55 +0100

    challenge solved

--- Code-wars/addTwoReversedNumbers.js
+++ Code-wars/addTwoReversedNumbers.js
@@ -10,13 +10,16 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
-var addTwoNumbers = function(l1) {
+const reverse=(l1)=>{
   let l1number="";
-    for(let i = l1.length-1; i>=0; i--){
-      l1number+=(l1[i].toString());
-      
-    }
-    return l1number;
+  for(let i = l1.length-1; i>=0; i--){
+    l1number+=(l1[i].toString()); 
+  }
+  return l1number;
+}
+var addTwoNumbers = function(l1, l2) {
+ result = parseInt(reverse(l1)) + parseInt(reverse(l2));
+ return (reverse(result.toString().split('')).split(''));
 };
 
-console.log(addTwoNumbers([2,4,3]));
+console.log(addTwoNumbers([2,4,3], [5,6,4]));
