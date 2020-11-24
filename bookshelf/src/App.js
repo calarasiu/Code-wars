f62a6b1a96a99ca5aa12038534178cc79deaf05d
cornelia chirila <cornelia.calarasiu@yahoo.it>
2020-11-24 13:04:55 +0000

    add description and home page image

--- bookshelf/src/App.js
+++ bookshelf/src/App.js
@@ -1,15 +1,15 @@
 import React from 'react'
-import './App.css';
+import './App.scss';
 import Navbar from './components/Navbar'
 import Description from './components/Description'
-
+ const image = "https://images.unsplash.com/photo-1602801831978-5ee390398a0d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=335&q=80"
 function App() {
   return (
     <div className="App">
       <Navbar />
       <div className="centralPage">
         <Description />
-        <img src="https://images.unsplash.com/photo-1602801831978-5ee390398a0d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=335&q=80" alt=""/>
+        <img src={image} className = "homeImage" alt=""/>
       </div>
 
     </div>
